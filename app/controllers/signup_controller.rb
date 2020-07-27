638746909c6223df7c78766c311e199b1116de8e
RongSon <shrimp509@hotmail.com>
2020-07-27 22:45:32 +0800

    Add signup function

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,12 @@
 class SignupController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+    puts "params: #{params}"
+    @user = User.new
+    @user.email = params[:email]
+    @user.password = params[:password]
+    @user.save
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/signin' => 'signin#index'
+
+
   get '/signup' => 'signup#index'
+  post '/signup' => 'signup#create'
 
   root 'homepage#index'
 end
