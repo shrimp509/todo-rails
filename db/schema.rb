ef9b7bd0bddefba3f2caf9528c88391cf6336bad
RongSon <shrimp509@hotmail.com>
2020-07-27 22:06:36 +0800

    Update schema with todo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_083946) do
+ActiveRecord::Schema.define(version: 2020_07_27_122905) do
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.boolean "done"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
